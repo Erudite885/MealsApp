4c968095f4e4ba7ff41df849656251c3954660ae
Erudite885 <erudite885@gmail.com>
2022-10-04 12:04:44 +0100

    modified the styling and component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react'
 
+import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components'
 import style from './App.module.css'
 
 const App = () => {
   return (
     <div className={style.wrapper}>
-        <h1>Hello, World!</h1>
+      <div className={`${style.navContainer}`}>
+        {/* can contain multiple styling */}
+        <div className={style.boxWidth}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={style.heroContainer}>
+        <div className={style.boxWidth}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={style.bodyContainer}>
+        <div className={style.boxWidth}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
