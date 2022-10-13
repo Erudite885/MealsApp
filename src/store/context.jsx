579eab55cea721f0ce1274ea6e4a80c1bed0ad32
Erudite885 <erudite885@gmail.com>
2022-10-13 03:45:45 +0100

    context: useEffect added

--- src/store/context.jsx
+++ src/store/context.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 
 const AppContext = React.createContext()
 
@@ -6,10 +6,15 @@ export const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
-const AppProvider = ({children}) => (
+const AppProvider = ({children}) => {
+    useEffect(() => {
+        console.log('fetch data here')
+    }, [])
+
+    return (
     <AppContext.Provider value={'hello'}>
         {children}
     </AppContext.Provider>
-)
+)}
 
 export {AppContext, AppProvider}
