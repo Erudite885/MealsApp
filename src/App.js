7fc022e85ca0abcc166d901be634fcc069882fa5
Erudite885 <erudite885@gmail.com>
2022-10-13 02:43:15 +0100

    App and App style initialized

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React from 'react'
 
-import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components'
+import {Search, Meals, Modal, Favorites} from './components/index';
 import style from './App.module.css'
 
 const App = () => {
   return (
     <div className={style.wrapper}>
-      <div className={`${style.navContainer}`}>
-        {/* can contain multiple styling */}
-        <div className={style.boxWidth}>
-          <Navbar />
-        </div>
-      </div>
-
-      <div className={style.heroContainer}>
-        <div className={style.boxWidth}>
-          <Hero />
-        </div>
-      </div>
-
-      <div className={style.bodyContainer}>
-        <div className={style.boxWidth}>
-          <Stats />
-          <Business />
-          <Billing />
-          <CardDeal />
-          <Testimonials />
-          <Clients />
-          <CTA />
-          <Footer />
-        </div>
-      </div>
+     <h1>Meals App </h1>
+     {/* <Search /> */}
+     {/* <Favorites /> */}
+     <Meals />
+     {/* <Modal /> */}
     </div>
   );
 }
