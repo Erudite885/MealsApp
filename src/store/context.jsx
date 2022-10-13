05f100afd6d6ffa198074c111e46e3495dc7b215
Erudite885 <erudite885@gmail.com>
2022-10-13 23:03:52 +0100

    context: merging the search to the all meals url

--- src/store/context.jsx
+++ src/store/context.jsx
@@ -7,19 +7,24 @@ export const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
-const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const randomMealsUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 const AppProvider = ({children}) => {
     
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const fetchMeals = async (url) => {
         setIsLoading(true);
         try {
-            const {data} = await axios(url);
-            setMeals(data.meals)
+            const { data } = await axios(url);
+            if (data.meals) {
+                setMeals(data.meals)
+            } else {
+                setMeals([])
+            }
             // const data = await res.json()
         } catch (error) {
             console.log(error)
@@ -28,11 +33,11 @@ const AppProvider = ({children}) => {
         // fetchData()
     }
     useEffect(() => {
-        console.log(fetchMeals(allMealsUrl))
-    }, [])
+       console.log(fetchMeals(`${allMealsUrl}${searchTerm}`));
+    }, [searchTerm])
 
     return (
-    <AppContext.Provider value={{ isLoading, meals }}>
+    <AppContext.Provider value={{ setSearchTerm, isLoading, meals }}>
         {children}
     </AppContext.Provider>
 )}
