ffe358c29e7fd6d6c966b6f5a8c315d27ae8b272
Erudite885 <erudite885@gmail.com>
2022-10-13 21:46:35 +0100

    context: url, meals & loading state, and fetching data by axios added to context store

--- src/store/context.jsx
+++ src/store/context.jsx
@@ -1,4 +1,5 @@
-import React, {useContext, useEffect} from 'react';
+import axios from 'axios'
+import React, {useContext, useEffect, useState} from 'react';
 
 const AppContext = React.createContext()
 
@@ -6,13 +7,32 @@ export const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const randomMealsUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 const AppProvider = ({children}) => {
+    
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMeals = async (url) => {
+        setIsLoading(true);
+        try {
+            const {data} = await axios(url);
+            setMeals(data.meals)
+            // const data = await res.json()
+        } catch (error) {
+            console.log(error)
+        }
+        setIsLoading(false);
+        // fetchData()
+    }
     useEffect(() => {
-        console.log('fetch data here')
+        console.log(fetchMeals(allMealsUrl))
     }, [])
 
     return (
-    <AppContext.Provider value={'hello'}>
+    <AppContext.Provider value={{ isLoading, meals }}>
         {children}
     </AppContext.Provider>
 )}
