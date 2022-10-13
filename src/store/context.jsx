7bc800f75d30a2cab5a892615d0c336e72567d21
Erudite885 <erudite885@gmail.com>
2022-10-13 03:12:35 +0100

    context: global context added

--- src/store/context.jsx
+++ src/store/context.jsx
@@ -2,8 +2,12 @@ import React, {useContext} from 'react';
 
 const AppContext = React.createContext()
 
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
 const AppProvider = ({children}) => (
-    <AppContext.Provider value='hello'>
+    <AppContext.Provider value={'hello'}>
         {children}
     </AppContext.Provider>
 )
