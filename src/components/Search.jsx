4b06b8ceb13bbcf27d7ff4d1438c425853365bab
Erudite885 <erudite885@gmail.com>
2022-10-13 23:02:22 +0100

    search: search state,

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGlobalContext } from "../store/context";
+
+import classes from "./search.module.css";
 
 const Search = () => {
+
+    const { setSearchTerm } = useGlobalContext()
+    const [text, setText] = useState('');
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+        console.log(text)
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text){
+            setSearchTerm(text);
+            setText('');
+        }
+    };
+
     return (
-        <h1>
-            Search
-        </h1>
+        <header className={classes.searchContainer}>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='text' 
+                    placeholder='search favorite meals' 
+                    className={classes.formInput}
+                    onChange={handleChange} 
+                    value={text}
+                />
+                <button 
+                    type='submit' 
+                    className={classes.btn}
+                >
+                        Search
+                </button>
+                <button 
+                    type='button' 
+                    className={classes.btnHipster}
+                >
+                    Random Suggestion
+                </button>
+            </form>
+        </header>
     )
 };
 
