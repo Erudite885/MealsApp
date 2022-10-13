f27f50b23e462309b48925c96b6993945c9fe2ec
Erudite885 <erudite885@gmail.com>
2022-10-13 23:20:18 +0100

    App initialized

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {Search, Meals, Modal, Favorites} from './components/index';
+import style from './App.module.css'
+
+const App = () => {
+  return (
+    <div className={style.wrapper}>
+     <h1>Meals App </h1>
+     <Search />
+     {/* <Favorites /> */}
+     <Meals />
+     {/* <Modal /> */}
+    </div>
+  );
+}
+
+export default App
