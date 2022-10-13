aa21ec34e0df87eb173fb21e1f30b6405af38ef8
Erudite885 <erudite885@gmail.com>
2022-10-13 03:14:01 +0100

    global context custom hooks added

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useGlobalContext } from '../store/context'
 
 const Meals = () => {
+    const context = useGlobalContext()
+    console.log(context)
     return (
         <h1>Meals</h1>
     )
