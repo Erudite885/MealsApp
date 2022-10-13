30d8f0f05d82ec4f41b140813f37b98e42a4678b
Erudite885 <erudite885@gmail.com>
2022-10-13 02:37:38 +0100

    index component

---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import Favorites from "./Favorites";
+import Meals from "./Meals";
+import Modal from "./Modal";
+import Search from "./Search";
+
+export {
+    Favorites, 
+    Meals, 
+    Modal, 
+    Search
+};
+
